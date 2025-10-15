e0d059f1086f562f6f3c1061bcfbeddf1658defe
Charlie Spitzer <charles.spitzer4@dont-nod.com>
2025-10-15 20:25:51 +0200

    Fixed POEntry equality method. (PLU-6284)

--- source/Karambolo.PO/POEntry.cs
+++ source/Karambolo.PO/POEntry.cs
@@ -83,8 +83,8 @@ namespace Karambolo.PO
             if (other is POSingularEntry sEntry)
                 return Key.FullEquals(sEntry.Key)
                     && Translation == sEntry.Translation
-                    && Comments != null && sEntry.Comments != null
-                    && Comments.SequenceEqual(sEntry.Comments);
+                    && (Comments == null && sEntry.Comments == null ||
+                        Comments.SequenceEqual(sEntry.Comments));
             return false;
         }
     }
